6ca2d297a4aa26d1c5f4600a1850c3d2416efaba
Murali981 <muraliju981@gmail.com>
2024-09-20 21:30:56 +0530

    We have successfully implemented all the backend routes by testing with postman API and also we have refactored our entire codebase by splitting them to multiple folders and store them to avoid complexity

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-const fs = require("fs");
-
 const express = require("express");
 
+const morgan = require("morgan");
+
+const tourRouter = require("./routes/tourRoutes");
+
+const userRouter = require("./routes/userRoutes");
+
 const app = express(); // It will add a bunch of methods to the app variable by calling the express() such that from app we can call
-// them
+// them.
+
+// 1) MIDDLEWARES
+
+app.use(morgan("dev")); // Here the morgan is a logging middleware and calling this morgan with dev as the argument will return
+// a function similar to the below middleware function
+
+app.use(express.json()); // Here the express.json() is the middleware and middleware is basically a function that can modify the
+// incoming request data . It is called middleware because it is standing between the request and the response . This is the step
+// that the request goes through while it is being processed. The body that the request sends is added to the request object.
+
+app.use(express.static(`${__dirname}/public`)); // Here the express.static() is used to serve the static file and we have given it as a public
+// folder form it has to serve the static files which are in the public folder like HTML , CSS and images files.
+// We can access the overview.html file from the google browser by using the path localhost:3000/overview.html. But here you may
+// get a doubt that why we are not giving the public in the this route to access the overview.html file (localhost:3000/public/overview.html)
+// because when we open up a URL that can't find in any of our routes then it will look in the public folder that we have defined. And
+// it sets that folder to the root . Now let's pretend that the root is now our public folder
+
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+  next();
+}); // This Middleware is applied to each and every request because we didn't specify any route here.
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 // app.get("/", (req, res) => {
 //   res.status(200).json({ msg: "Hello, World!", app: "Natours" });
@@ -14,33 +44,29 @@ const app = express(); // It will add a bunch of methods to the app variable by 
 //   res.send("You can post to this endpoint...");
 // });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-); // Here __dirname is the folder where the current
-// script is located which is our app.js file which is located in the starter folder which represents our current directory name.
-// The JSON.parse(tours-simple.json) function will take the tours-simple.json data as input and it will automatically converted to JSON
-// object (or) an array of javascript objects
+// 2.) ROUTE HANDLERS
 
-app.get("/api/v1/tours", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours: tours, // (or) you can simply leave it as tours . In tours:tours the right side tours will represent the tours variable
-      // that is coming from the fs.readFileSync() function and the left side tours is representing the tours as a resource in the
-      // path "/api/v1/tours"
-    },
-  }); // This type of formating the response data coming from the server is "JSEND data specification"
-}); // This API endpoint will send back all the tours as a response back to the client. Here
-// tours is the resource . Here in this application we are building an API as well . The tours data is stored in the dev-data folder
-// and inside the data folder and then inside the data folder there is tours-simple.json file which contains all the information about
-// the tours that we are selling in our website(dev-data -> data -> tours-simple.json) . Only the call-back function in this route
-// will run inside the event loop . So in this you cannot have any blocking code. Please remember that the data we are reading is
-// from a file based API because we are getting the tours data from the tours-simple.json file which is in our current application.
-// And later we will store this tours JSON data in mongoDB and get that data back from the mongoDB as a response back to the client.
+// app.get("/api/v1/tours", getAllTours); // This API endpoint will send back all the tours as a response back to the client. Here
+// // tours is the resource . Here in this application we are building an API as well . The tours data is stored in the dev-data folder
+// // and inside the data folder and then inside the data folder there is tours-simple.json file which contains all the information about
+// // the tours that we are selling in our website(dev-data -> data -> tours-simple.json) . Only the call-back function in this route
+// // will run inside the event loop . So in this you cannot have any blocking code. Please remember that the data we are reading is
+// // from a file based API because we are getting the tours data from the tours-simple.json file which is in our current application.
+// // And later we will store this tours JSON data in mongoDB and get that data back from the mongoDB as a response back to the client.
 
-const PORT = 3000;
+// app.get("/api/v1/tours/:id", getTour);
 
-app.listen(PORT, () => {
-  console.log("App is listening on port 3000");
-});
+// app.post("/api/v1/tours", createTour);
+
+// app.patch("/api/v1/tours/:id", updateTour);
+
+// app.delete("/api/v1/tours/:id", deleteTour);
+
+// 3.) ROUTES
+
+app.use("/api/v1/tours", tourRouter); // For this route we want to apply the tourRouter middleware
+app.use("/api/v1/users", userRouter); // For this route we want to apply the userRouter middleware
+
+// 4.) START THE SERVER
+
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,133 @@
+const fs = require("fs");
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+); // Here __dirname is the folder where the current
+// script is located which is our app.js file which is located in the starter folder which represents our current directory name.
+// The JSON.parse(tours-simple.json) function will take the tours-simple.json data as input and it will automatically converted to JSON
+// object (or) an array of javascript objects
+
+exports.checkId = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    // If the tour is undefined(!tour) then simply return the 404 response with invalid ID as a response from the server
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    }); // If we didn't have this return statement here then the express would send this response back but it will still continue
+    // running the code in this function . So after sending the response it will still hit the next() function below and it
+    // would move on to the next() middleware and then it will send another response to the client. So the above return statement
+    // makes sure that it will finish and it makes sure that it will never call this next() function again
+  }
+  next(); // If the id is valid then we call the next middleware function
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Missing name or price",
+    });
+  }
+  next(); // If the body is valid then we call the next middleware function
+};
+
+exports.getTour = (req, res) => {
+  console.log(req.params); // req.params is where all the parameters of all the variables that we define here are stored . The variables
+  // in the above URL are called parameters and they are available in req.params which are readily available such that we can use them.
+
+  const id = req.params.id * 1; // when we multiply a string that looks like a number({id : "5"}) then it will automatically convert
+  // this string to a number.
+
+  //   const tour = tours.find((el) => el.id === Number(req.params.id));
+  const tour = tours.find((el) => el.id === id);
+
+  //   if (id > tours.length) {
+  //     return res.status(404).json({
+  //       status: "fail",
+  //       message: "Invalid ID",
+  //     });
+  //   }
+
+  //   if (!tour) {
+  //     // If the tour is undefined(!tour) then simply return the 404 response with invalid ID as a response from the server
+  //     return res.status(404).json({
+  //       status: "fail",
+  //       message: "Invalid ID",
+  //     });
+  //   }
+
+  res.status(200).json({
+    status: "success",
+    // results: tours.length,
+    data: {
+      tour,
+    },
+  }); // This type of formating the response data coming from the server is "JSEND data specification"
+};
+
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: "success",
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours: tours, // (or) you can simply leave it as tours . In tours:tours the right side tours will represent the tours variable
+      // that is coming from the fs.readFileSync() function and the left side tours is representing the tours as a resource in the
+      // path "/api/v1/tours"
+    },
+  }); // This type of formating the response data coming from the server is "JSEND data specification"
+};
+
+exports.createTour = (req, res) => {
+  // Here the request object will hold all the data related to our post route (or) request that we are sending to the server.
+  // Out of the box , Express does not put the body data on the request object . So inorder to have the data available , We have to
+  // pass something called as middleware . So we aill include the middleware on the top of the file.
+  //
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body); // This allows us to create a new object by merging two existing objects together
+  console.log(newTour);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours), // Here tours is normal plain javascript object and so we need to convert  that
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  ); // We are inside of a callback function that is gonna run in  the event loop as we can never ever block the
+  // event loop . So that's why are writing the writeFile rather than writing writeFileSync() instead
+};
+
+exports.updateTour = (req, res) => {
+  //   if (req.params.id > tours.length) {
+  //     // If the tour is undefined(!tour) then simply return the 404 response with invalid ID as a response from the server
+  //     return res.status(404).json({
+  //       status: "fail",
+  //       message: "Invalid ID",
+  //     });
+  //   }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour here>",
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    // If the status code is 204 then it means that there is no content as we will not send any result back to the client after a
+    // particular tour got deleted
+    status: "success",
+    data: {
+      tour: null,
+    },
+  });
+};
